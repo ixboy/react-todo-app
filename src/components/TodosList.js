87a8b3a360dd645e7799457469dd918c89527858
ixboy <ismaelixboy@gmail.com>
2021-11-09 23:49:41 +0100

    add dummy data into the list

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,1 +1,16 @@
+import React from 'react'
+
+export class TodosList extends React.Component {
+  render() {
+    return (
+      <ul>
+      {this.props.todos.map(todo => (
+        <li>{todo.title}</li>
+      ))}
+    </ul>
+    )
+  }
+}
+
+export default TodosList
 
