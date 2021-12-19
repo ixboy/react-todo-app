821a5b3aef9f7ca50e14e19a9667e733f8576eb5
ixboy <ismaelixboy@gmail.com>
2021-12-19 02:22:40 +0100

    change file structure and put classbased components into one file

---
+++ src/classBased/components/InputTodo.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+
+class InputTodo extends Component {
+  state = {
+    title: "",
+  };
+
+  onChange = e => {
+  this.setState({
+  [e.target.name]: e.target.value,    
+  })
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    if(this.state.title.trim()){
+      this.props.addTodoProps(this.state.title);
+      this.setState({    title: ""  });  
+    }else{
+      alert("Please write your Todo")
+    }
+    
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          className="input-text"
+          placeholder="Add Todo..."
+          value={this.state.title}
+          onChange={this.onChange}
+          name="title"
+        />
+        <button className="input-submit">Submit</button>
+      </form>
+    );
+  }
+}
+export default InputTodo;
